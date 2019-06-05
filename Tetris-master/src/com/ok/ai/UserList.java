129d15b77c836c0ac3e0506a430093def646a956
leeay0219 <35085704+leeay0219@users.noreply.github.com>
2019-06-05 15:22:33 +0900

    Update UserList.java

--- Tetris-master/src/com/ok/ai/UserList.java
+++ Tetris-master/src/com/ok/ai/UserList.java
@@ -1,11 +1,11 @@
 package com.ok.ai;
 
-import com.ok.ai.UserList;
+import com.ok.ai.UserList; 
 
 public class UserList implements Comparable<UserList>{
 	String id;
 	int score;
-	public UserList(String name, int sc){
+	public UserList(String name, int sc){ //get user ID & Score, return them
 		id = name;
 		score = sc;
 	}
